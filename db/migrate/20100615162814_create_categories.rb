491ff60ef2c61280bd9d59d8e3559c370054a9f2
Alexander von Gluck <kallisti5@unixzen.com>
2010-06-15 11:55:03 -0500

    changes to db migrations to create 'default' categories

--- db/migrate/20100615162814_create_categories.rb
+++ db/migrate/20100615162814_create_categories.rb
@@ -4,8 +4,27 @@ class CreateCategories < ActiveRecord::Migration
 	t.string	:name
         t.text		:description
     end
+
 	Category.create	:name => "Development",
 			:description => "Applications or utilities relating to Haiku software development"
+	Category.create	:name => "Drivers",
+			:description => "Haiku compatible drivers for your system"
+	Category.create	:name => "Engineering",
+			:description => "Engineering utilities"
+	Category.create	:name => "Games",
+			:description => "Games for the Haiku operating system."
+	Category.create	:name => "Graphics",
+			:description => "Applications to view or manipulate photos"
+	Category.create	:name => "Internet",
+			:description => "Applications or utilities relating to the world wide web"
+	Category.create	:name => "Multimedia",
+			:description => "Sound or video related applications."
+	Category.create	:name => "Network",
+			:description => "Local or remote network related applications and utilities."
+	Category.create	:name => "Office",
+			:description => "Document management or business related applications."
+	Category.create	:name => "Utilities",
+			:description => "General system utilities"
   end
 
   def self.down
