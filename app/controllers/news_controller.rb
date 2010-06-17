722b52aa9309cd6cbba969c0133dfc2ead278def
Alexander von Gluck <kallisti5@unixzen.com>
2010-06-17 18:01:00 -0500

    added twitter news item support

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,9 +1,24 @@
 class NewsController < ApplicationController
-  # GET /news
-  # GET /news.xml
-  def index
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
+
+	  # GET /news
+	  # GET /news.xml
+	def index
+
+		gem "twitter4r"
+		require 'ostruct'
+
+		begin
+		twitter = Twitter::Client.new(:login => "haikufire", :password => "PASS")
+		@twitter_timelines = twitter.timeline_for(:user, :id => "haikufire")
+
+		rescue
+			@twitter_timelines = Array.new
+			@twitter_timelines << OpenStruct.new(:text => "Twitter news items are currenty offline")
+		end
+
+
+		respond_to do |format|
+			format.html # index.html.erb
+		end
+	end
 end
