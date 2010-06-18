719725a8d5b09a17d1b3ccc4437c46d73e03201f
Alexander von Gluck <kallisti5@unixzen.com>
2010-06-18 12:43:19 -0500

    updates to side bar, added better display of news tweets

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,9 +1,7 @@
 class NewsController < ApplicationController
 
-	  # GET /news
-	  # GET /news.xml
 	def index
-
+		# Gather news items from Twitter and prepare them for the view
 		gem "twitter4r"
 		require 'ostruct'
 
@@ -12,13 +10,19 @@ class NewsController < ApplicationController
 		@twitter_timelines = twitter.timeline_for(:user, :id => "haikufire")
 
 		rescue
+			# If there is a problem, create a failure news item.
+			# This prevents rails traces based on Twitter Failwhales
+
 			@twitter_timelines = Array.new
-			@twitter_timelines << OpenStruct.new(:text => "Twitter news items are currenty offline")
+			@twitter_timelines << OpenStruct.new(	:text => "Twitter is currently unavailable",
+								:created_at => Time.now.asctime << " CST",
+								:user => OpenStruct.new(:screen_name => "Failwhale"))
 		end
 
 
 		respond_to do |format|
 			format.html # index.html.erb
 		end
+
 	end
 end
