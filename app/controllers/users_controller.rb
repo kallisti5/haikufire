69240f565e790c48790d39072aa8c0cc30b60a0b
Alexander von Gluck <kallisti5@unixzen.com>
2010-07-06 12:59:08 -0500

    work on new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,11 @@ class UsersController < ApplicationController
 	if valid_user
 		#creates a session with username
 		session[:user_id]=valid_user.login
+
+		# update last login time
+		valid_user.last_login = Time.now
+		valid_user.save
+
 		#redirects the user to our private page.
 		redirect_to :action => 'profile', :id => @user.login
 	else
@@ -56,11 +61,15 @@ class UsersController < ApplicationController
   end
 
   def create
+
     @user = User.new(params[:user])
+
+    @user.role = 2
+    @user.created_at = Time.now
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to( @user, :notice => 'User was successfully created.') }
+	format.html { redirect_to :action => 'profile', :id => @user.login }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+	validates_presence_of :login, :name
+	validates_uniqueness_of :login
+
+	def password=(password)
+		require 'digest/md5'
+		write_attribute :password, Digest::MD5.hexdigest( password )
+	end
 end
