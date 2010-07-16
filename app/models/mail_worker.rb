ed8900218bea7959bbc263b323c04efa79b8a244
Alexander von Gluck <kallisti5@unixzen.com>
2010-07-16 17:18:58 -0500

    fixed problem in mail worker, made user action box on profile not appear unless user is logged in

--- app/models/mail_worker.rb
+++ app/models/mail_worker.rb
@@ -3,7 +3,7 @@ class MailWorker < ActionMailer::Base
    def verification(recipient, validation_hash, sent_at = Time.now)
       @validation_hash = validation_hash
       @subject = subject
-      @recipients = recipient
+      @recipient = recipient
       @from = 'MailWorker@haikufire.com'
       @sent_on = sent_at
         @body["title"] = 'Welcome to Haikufire!'
