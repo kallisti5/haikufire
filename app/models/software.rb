929fb128f25e94de7ecaddee286c647b52397df3
Alexander von Gluck <kallisti5@unixzen.com>
2010-07-16 17:09:43 -0500

    new button on user page to submit software

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,9 +1,11 @@
 class Software < ActiveRecord::Base
-	belongs_to :category
-
-	validates_uniqueness_of :title
+	#belongs_to :category
 
 	validates_presence_of :title
 	validates_presence_of :category_id
 	validates_presence_of :author
+	validates_presence_of :description
+	validates_presence_of :compiled
+
+	validates_uniqueness_of :title
 end
