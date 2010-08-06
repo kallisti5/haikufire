b6f475cccba7ffd8224ed3e54b5890ac8c406fbe
Alexander von Gluck <kallisti5@unixzen.com>
2010-08-06 10:40:42 -0500

    cleanup of software model

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,5 +1,4 @@
 class Software < ActiveRecord::Base
-	#belongs_to :category
 
 	validates_presence_of :title
 	validates_presence_of :category_id
@@ -9,8 +8,16 @@ class Software < ActiveRecord::Base
 
 	validates_uniqueness_of :title
 
-	# paperclip magic
+	### Start paperclip magic
 	# in the future Amazon s3 image storage is possible
+		# :storage => :s3,
+		# :bucket => "bucket name",
+		# :s3_credentials => {
+		#	:access_key_id => "access key id",
+		#	:secret_access_key => "secret access key"
+		# },
+
+	# paperclip application icon
 	has_attached_file :icon,
 		:default_url => "/assets/no_icon.png",
 		:url => "/assets/:id/:style/:basename.:extension",
@@ -18,11 +25,14 @@ class Software < ActiveRecord::Base
 		:styles => {
 			:icon => "32x32#"}
 
+	# paperclip application screenshot
 	has_attached_file :screenshot,
 		:default_url => "/assets/no_screenshot.png",
 		:url => "/assets/:id/:style/:basename.:extension",
 		:path => ":rails_root/public/assets/:id/:style/:basename.:extension",
 		:styles => {
                         :shot_sm => "128x128#"}
+
+	### End paperclip magic
 
 end
