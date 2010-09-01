1f17810e4d2339a2548a045a9930ce01a54f048a
Alexander von Gluck <kallisti5@unixzen.com>
2010-09-01 16:38:42 -0500

    initial init of rails3 environment, stuff to work on backed up as .RAILS2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haikufire::Application.config.secret_token = '36d1b472468c340c3e7f3151943b58ff4fcea66b9e0ac3599adba7e21c5c4a19286b8405270530a11b01189dd7fe5c3fded90c55eb7cd8949eaa8e635ef126a9'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_haikufire_session',
-  :secret      => 'b87d09ff8eb150696b50b62da1349a6e8678825c613e3dd13fc93618480876e1a6e575f70544edce0d5410e3780ab3d21734b686fdb5a593316a1a53eb0c8d7a'
-}
+Haikufire::Application.config.session_store :cookie_store, :key => '_haikufire_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Haikufire::Application.config.session_store :active_record_store
