5ae20ba6f64220dc9221c946ea5d3c121cd07617
Alexander von Gluck IV <kallisti5@unixzen.com>
2010-10-03 00:50:29 -0500

    user model tab cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@ require 'digest/sha2'
 
 class User < ActiveRecord::Base
 
+	# Temporary in memory storage for virtual plaintext password and confrimation password
 	attr_accessor :password, :password_confirmation
 
 	has_many :softwares
 
 	validates_length_of	:username, :within => 3..32, :message => 'must be between 3 and 32 characters'
 
-  validates_format_of     :username,
-        :with       => /^[a-z0-9]+$/i,
-        :message    => 'must only contain valid characters ( eg. a-z 0-9 )'
+	validates_format_of     :username,
+		:with       => /^[a-z0-9]+$/i,
+		:message    => 'must only contain valid characters ( eg. a-z 0-9 )'
 
 	validates_uniqueness_of :username
 
@@ -21,8 +22,8 @@ class User < ActiveRecord::Base
 
 	validates_uniqueness_of :email
 	validates_format_of     :email,
-				:with       => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
-				:message    => 'must be valid'
+		:with       => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+		:message    => 'must be valid'
 
 	apply_simple_captcha
 
@@ -42,13 +43,9 @@ private
 	end
 
 	def hash_password
-		# First reset the salt to a new random string.  You could choose a
-		# longer string here but for a salt, 8 bytes of randomness is probably
-		# fine.  Note this uses SecureRandom which will use your platform's secure
-		# random number generator.
+		# Generate a new salt for this password hash
 		self.pass_salt = ActiveSupport::SecureRandom.base64(16)
-		# Now calculate the hash of the password, with the salt prepended, store
-		# store that in the database
+		# Convert salted password into a hash, store it as well
 		self.pass_hash = Digest::SHA2.hexdigest(self.pass_salt + @password)
 	end
 
