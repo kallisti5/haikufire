522b58c04d7921a2cc583e96e8b91c16ec205fc3
Alexander von Gluck IV <kallisti5@unixzen.com>
2010-11-18 09:32:11 -0600

    route corrections, added links to downloads management page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Haikufire::Application.routes.draw do
-  get "downloads/edit"
-
-  get "downloads/update"
-
-  get "downloads/delete"
-
-  get "downloads/destroy"
-
-  get "downloads/get"
-
   match '/' => 'news#index'
   resources :categories
   resources :users
   resources :softwares
+  resources :downloads
   match '/auth/login' => 'users#login', :as => :login
   match '/auth/logout' => 'users#logout', :as => :logout
   match '/simple_captcha(/:action)' => 'simple_captcha', :as => :simple_captcha
